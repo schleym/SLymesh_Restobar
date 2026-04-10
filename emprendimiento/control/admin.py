53da844853f0f82e9f926281ede376ab53bdb738
schleym <estebanmelladoalegria@gmail.com>
2026-04-10 16:11:30 -0400

    Registrar modelos en admin

--- emprendimiento/control/admin.py
+++ emprendimiento/control/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Coctel, Mocktail, Pedido, Producto, Receta, Usuario, VariableCoctel
 
-# Register your models here.
+admin.site.register(Coctel)
+admin.site.register(Mocktail)
+admin.site.register(Pedido)
+admin.site.register(Producto)
+admin.site.register(Receta)
+admin.site.register(Usuario)
+admin.site.register(VariableCoctel)
